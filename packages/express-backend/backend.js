f83b464a88d205f96662772bc06d107c65139a7d
kendricktran <kendrick.k.tran@gmail.com>
2024-04-18 18:49:14 -0700

    add index user get

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,6 +5,10 @@ const port = 8000;
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 const users = {
   users_list: [
     {
@@ -35,9 +39,37 @@ const users = {
   ]
 };
 
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
 
-app.get("/", (req, res) => {
-  res.send(users);
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+
+
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
